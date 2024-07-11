17d6883aa43f2959a7bf8ac548f60e83b9e3fc83
Ungraduate1394 <101564335+Userdied1394@users.noreply.github.com>
2024-07-11 09:07:31 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -49,9 +49,9 @@ const Contact = () => {
 				EMAIL_JS_TEMPLATE_ID,
 				{
 					from_name: username,
-					to_name: "Nithin Manda",
+					to_name: "Hemagiri Naidu",
 					reply_to: user_email,
-					to_email: "goudnithin77@gmail.com",
+					to_email: "hemagiri1394.in@gmail.com",
 					message: user_message,
 				},
 				EMAIL_JS_PUBLIC_KEY
